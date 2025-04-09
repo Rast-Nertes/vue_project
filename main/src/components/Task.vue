0b17480ba785aab1f330579a7f9559f76d6981bc
Eldar <eldarkad122@gmail.com>
2025-04-09 22:05:09 +0300

    append some tasks

--- main/src/components/Task.vue
+++ main/src/components/Task.vue
@@ -1,19 +1,27 @@
+<!-- src/components/Task.vue -->
 <script setup>
 import { ref, watchEffect } from 'vue';
 import { useRoute } from 'vue-router';
+import tasksData from '../data/taskData.js';
 
 const route = useRoute();
 const taskComponent = ref(null);
+const taskProps = ref({});
 
 watchEffect(async () => {
   try {
     taskComponent.value = (await import(`../views/Task${route.params.id}.vue`)).default;
+    taskProps.value = tasksData[route.params.id] || {};
   } catch (e) {
-    taskComponent.value = null; // Если файла нет, показываем заглушку
+    taskComponent.value = null;
+    taskProps.value = {};
   }
 });
 </script>
 
 <template>
-  <component :is="taskComponent || 'div'">Задача не найдена</component>
+  <component 
+    :is="taskComponent || 'div'"
+    v-bind="taskProps"
+  >Задача не найдена</component>
 </template>
